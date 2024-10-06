2c3509efbc827378ce8d8780881475a9df1c6a4b
Arester <kiennungloz123@gmail.com>
2024-10-06 09:48:12 +0700

    sửa defect report 24 thêm thông báo giá bìa không được để trống

--- quantri/asset/js/script.js
+++ quantri/asset/js/script.js
@@ -344,6 +344,10 @@ function formValidateProduct(tuasach, nxb, idNCC, giabia, tacgia, namxb, idTL, m
     }
 
     // giabia
+    if (giabia == "") {
+        alert = "<span class='red'>Giá bìa không được để trống.</span>";
+        return alert
+    }
     if(isNaN(giabia) || giabia <= 0){
         alert = "<span class='red'>Giá bìa không hợp lệ.</span>";
         return alert;
