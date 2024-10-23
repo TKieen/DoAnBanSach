e58841345b21d29963631bd3b736fe3e072338d6
LE NGOC THAO VY <thaovy3724@gmail.com>
2024-10-23 08:17:20 +0700

    kiem tra ton kho khi them vao gio hang

--- controller/addToCart.php
+++ controller/addToCart.php
@@ -1,7 +1,18 @@
 <?php
+include '../lib/connect.php';
+require_once "../lib/session.php";
+require '../model/product.php';
+
     if(!empty($_SESSION['user']['id'])) {
         if(isset($_POST['addToCart'])) {
+        // kiem tra asyncronus ton kho
             $id = $_POST['idSach'];
+            $ketqua = getTonKhoByID($id);
+            if($ketqua['tonkho'] <= 0){
+                echo json_encode(['success' => false, 'direct' => false]);
+                return;
+            }
+
             $img = $_POST['img'];
             $name = $_POST['name'];
             $originalPrice = $_POST['originalPrice'];
@@ -21,16 +32,17 @@
             //Kiểm tra sản phẩm có tồn tại trong giỏ hàng hay không?
             //Nếu có thì chỉ cập nhật lại số lượng
             $i = 0;
-            foreach ($_SESSION['cart'] as $item) {
-                if($item[2] === $name) {    //Chỉnh sửa: so sánh id thay vì tên
-                    //cập nhật trực tiếp trên giỏ hàng
-                    $qty_new = $qty + $item[5];
-                    $_SESSION['cart'][$i][5] = $qty_new;
-                    $isExists = 1;
-                    break;
+            if(!empty($_SESSION['cart']))
+                foreach ($_SESSION['cart'] as $item) {
+                    if($item[2] === $name) {    //Chỉnh sửa: so sánh id thay vì tên
+                        //cập nhật trực tiếp trên giỏ hàng
+                        $qty_new = $qty + $item[5];
+                        $_SESSION['cart'][$i][5] = $qty_new;
+                        $isExists = 1;
+                        break;
+                    }
+                    $i++;
                 }
-                $i++;
-            }
             //Nếu không thì thêm một sản phẩm mới
             if($isExists == 0) {
                 $product = array($id, $img, $name, $originalPrice, $salePrice, $qty);
@@ -40,11 +52,10 @@
                 array_push($_SESSION['cart'], $product);
             }
             
-            header("Location:index.php?page=cart");
+            echo json_encode(['success' => true]);
         }
     }
     else {
-        header("Location:index.php?page=signIn");
-        echo "<script>alert('Hãy đăng nhập để có thể mua hàng')</script>";
+        echo json_encode(['success' => false, 'direct' => true]);
     }
 ?>
