ce0a8504ee44e11056a0955099f14d47f84efba0
LE NGOC THAO VY <thaovy3724@gmail.com>
2024-11-30 15:10:03 +0700

    sua controller signup chuyen qua xu ly ben js

--- controller/signUp.php
+++ controller/signUp.php
@@ -26,25 +26,13 @@
         if (check_isEmpty($phone)) {
             echo json_encode(['success'=>false, 'message'=>'Số điện thoại không được để trống']); return;
         }
-        if (isFullNameValid($fullname) == false) {
-            echo json_encode(['success'=>false, 'message'=>'Họ tên không hợp lệ']); return;
-        }
-        if (check_email_is_valid($email)) {
-            echo json_encode(['success'=>false, 'message'=>'Email không hợp lệ']); return;
-        }
-        if (strlen($email) < 10 || strlen($email) > 254) {
-            echo json_encode(['success'=>false, 'message'=>'Email phải có độ dài từ 10 đến 254 ký tự']); return;
-        }
+        
         if (check_email_is_existed($email)) {
             echo json_encode(['success'=>false, 'message'=>'Email đã tồn tại']); return;
         }
 
         if (check_phone_is_existed($phone)) {
             echo json_encode(['success'=>false, 'message'=>'Số điện thoại đã tồn tại']); return;
-        }
-
-        if (strlen($password) < 8 || strlen($password) > 20) {
-            echo json_encode(['success'=>false, 'message'=>'Mật khẩu phải có độ dài từ 8 đến 20 ký tự']); return;
         }
 
         if (check_password_is_unmatched($password, $rPassword)) {
